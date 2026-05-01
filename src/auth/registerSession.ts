f52aa002eeabd9958a0d121fc135faa52f1e91e4
Crash-- <quentin.valmori@gmail.com>
2026-05-01 15:26:29 +0200

    feat(auth): registerSession exchanges registerToken for access/refresh

---
+++ src/auth/registerSession.ts
@@ -0,0 +1,32 @@
+import CozyClient from 'cozy-client'
+
+import { Session } from './types'
+
+interface RegisterParams {
+  fqdn: string
+  registerToken: string
+}
+
+export const registerSession = async ({ fqdn, registerToken }: RegisterParams): Promise<Session> => {
+  const uri = `https://${fqdn}`
+  const client = new CozyClient({ uri })
+
+  const stackClient = client.getStackClient()
+  const oauthOptions = {
+    clientName: 'Twake Drive Mobile',
+    softwareID: 'twake-drive-mobile',
+    redirectURI: 'twakedrive://',
+    clientKind: 'mobile',
+    clientURI: 'https://twake.app',
+    scopes: ['io.cozy.files', 'io.cozy.files.shared-with-me']
+  }
+
+  await stackClient.register(oauthOptions)
+  const token = await stackClient.fetchAccessToken(registerToken)
+
+  return {
+    uri,
+    accessToken: token.accessToken,
+    refreshToken: token.refreshToken
+  }
+}
